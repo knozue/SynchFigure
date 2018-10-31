23dd64bf09bc330ec7bc865054a5820c6febab77
Kazunari Nozue <knozue@ucdavis.edu>
2018-10-30 22:59:23 -0700

    103018v3 calculate "age", which is a new x-axis. etc

--- server.R
+++ server.R
@@ -45,11 +45,11 @@ vlookup <- function(ref, #the value or values that you want to look for
   dim(output) <- dim(ref)
   output
 }
-##
+## separate date data
 Fig.data.s<-Fig.data %>% as_tibble() %>% mutate(DateTime2=mdy(DateTime),year=year(DateTime2))
 Fig.data.s<-Fig.data.s %>% mutate(name2=toupper(name),team=toupper(team))
 Fig.data.s.summary <- Fig.data.s %>% group_by(name2,team) %>% summarise(average=mean(score))
-
+# To distinguish 2006 and 2007 based on age group 11-12 in 20117 and 2018
 swimmer2006<-Fig.data.s %>% filter(year %in% c(2017, 2018), category %in% "11-12",competition %in% c("Invitational Figures San Francisco","West Region B championships")) %>% 
   group_by(name2, competition,category) %>% 
   summarise(num=n()) %>% filter(num==2) %>% 
@@ -60,26 +60,35 @@ swimmer2007<-Fig.data.s %>% filter(year==2018,competition %in% c("Invitational F
 Fig.data.s.summary$born<-" "
 Fig.data.s.summary[Fig.data.s.summary$name2 %in% swimmer2006$name2,"born"]<-"2006"
 Fig.data.s.summary[Fig.data.s.summary$name2 %in% swimmer2007$name2,"born"]<-"2007"
+# To distinguish 2007 and 2008 based on age group 11-12 in 2016 and 2017
+
+
+
+# calculate average score per swimmer
 Fig.data.s.summary <- Fig.data.s.summary %>% mutate(average=round(average,2))
-##
+# combine and calculate age
+  Fig.data.ss<-Fig.data.s %>% left_join(Fig.data.s.summary[,c("name2","born")],by="name2") %>% mutate(age=interval(start=parse_date_time(born, orders = "Y"),end=DateTime2)/dyears(1)) %>% 
+  select(-name,-team.abb,-DateTime2,-year)
+# start function()
 function(input, output) {
   # choose columns to display
   output$mytable1 <- DT::renderDataTable({
-   # DT::datatable(Fig.data.s.summary %>% DT::formatRound("average",2)) # did not work
+     #DT::datatable(Fig.data.s.summary) %>% DT::formatRound("average",2)) # did not work
+   # Fig.data.ss %>% mutate(age=round(age,2)) %>% select(-DateTime,-category) %>% DT::datatable()
     DT::datatable(Fig.data.s.summary)
-  })
+      })
    # graph
   pl <- reactive({
     # draw ggpllot
     #ggplot(Fig.data.s,aes(x=DateTime2,y=score,color=name2))  + geom_point() + geom_line(aes(linetype=team))  + theme_bw() + theme(legend.position="none")
     print("input$mytable1_rows_selected is")
     print(input$mytable1_rows_selected)
-    Fig.data.s.selected<- Fig.data.s %>% filter(name2 %in% as_vector(Fig.data.s.summary[input$mytable1_rows_selected,"name2"]))
+    Fig.data.ss.selected<- Fig.data.ss %>% filter(name2 %in% as_vector(Fig.data.s.summary[input$mytable1_rows_selected,"name2"]))
     # geom_dl is used for labeling line with names
-    ggplot(Fig.data.s.selected,aes(x=DateTime2,y=score))  + geom_point() + geom_line(aes(group=name2,color=team))  + 
+    ggplot(Fig.data.ss.selected,aes(x=age,y=score))  + geom_point() + geom_line(aes(group=name2,color=born))  + 
       geom_dl(aes(label = name2), method = list(dl.trans(x = x + .1), "smart.grid")) +
       #geom_dl(aes(label = name2), method = list(dl.trans(x = x + .1), "first.points",rot=30)) +
-            expand_limits(x = as.Date("2018-10-01")) +
+      #      expand_limits(x = as.Date("2018-10-01")) +
       theme_bw() + theme(legend.position="none")
       })
   output$plot <- renderPlot({pl()})
--- ui.R
+++ ui.R
@@ -1,5 +1,7 @@
 library(shiny)
 fluidPage(
-  title = "Examples of DataTables",
-    fluidRow(column(width = 4,DT::dataTableOutput("mytable1")),column(width = 6,plotOutput("plot",height = "900px")))
+  
+  title = "Figure",
+    fluidRow(column(width = 5,DT::dataTableOutput("mytable1")),
+             column(width = 5,plotOutput("plot",height = "900px")))
 )
